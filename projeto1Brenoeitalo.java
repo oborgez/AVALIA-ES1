e0a7b3437878118a9646bb0aa204a65bb7f95dc2
oborgez <oborgez13@outlook.com>
2025-06-30 14:15:59 -0300

    Add files via upload

---
+++ projeto1Brenoeitalo.java
@@ -0,0 +1,15 @@
+public class projeto1Brenoeitalo {
+    public static void main(String[] args) {
+        int numero1 = 40;
+        int numero2 = 20;
+        int numero3 = 30;
+
+        int resultdSubtracao = numero1 - numero2 - numero3;
+       // int resultdAdicao = numero1 + numero2;
+       // int resultdDivisao = numero1 / numero2 * numero3 + numero2 - numero1;
+        //int resultdMulti = nunmero1 * numero2;
+
+        System.out.println("o resultodo da operação foi:" + resultdSubtracao);
+        System.out.println("pelos meus calculos é " +);
+    }
+}
